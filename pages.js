89ead4940ca7ec692659a36a67b203752e7b4ec6
ApolloZhu <public-apollonian@outlook.com>
2019-12-02 18:11:06 -0800

    LHD style update

--- pages.js
+++ pages.js
@@ -29,50 +29,94 @@ const PageDataHelper =
         
         "Events":
         `
-            <h1>Local Hack Day: Build @ UW</h1>
-            <hr/>
-            <h2>Overview</h2>
-            <p>On December 7<sup>th</sup> from 8:00 AM to 9:00 PM, the University of Washington's
-               Mobile Development Club will be hosting a hackathon!</p>
-            <button class = "hugeButton" onclick = "window.open('https://localhackday.mlh.io/build/locations/2874/');">Register</button>
-            
-            <h2>Planned Schedule</h2>
-            <ul style = "font: 11pt mono, monospace, courier, sans;">
+        <h1>Local Hack Day: Build @ UW</h1>
+        <p>On December 7<sup>th</sup> from 8:00 AM to 9:00 PM, 
+           Mobile Development Club will be hosting a hackathon! Come to get <strong>swags and laptop stickers</strong>, study for finals together, or learn something new through our workshops, build something awesome, and win prizes like <strong>Amazon Echo Dot</strong>, and more!</p>
+        <button class = "hugeButton" onclick = "window.open('https://localhackday.mlh.io/build/locations/2874/');">Register for FREE</button>
+
+        <div>
+        <h2>Schedule</h2>
+            <ul class="lhdSchedule">
                 <li><b>8:00 &nbsp;AM</b> Check-in and registration!</li>
                 <li><b>8:45 &nbsp;AM</b> Opening Ceremony</li>
-                <li><b>10:00 AM</b> <i>Optional Workshop:</i> iOS Development with Swift and SwiftUI</li>
-                <li><b>10:00 AM</b> <i>Optional Workshop:</i> CSE 142 Study Group</li>
-                <li><b>11:00 AM</b> <i>Optional Workshop:</i> Android Development with Java</li>
-                <li><b>11:00 AM</b> <i>Optional Workshop:</i> How to Collaborate with GitHub</li>
-                <li><b>1:00 &nbsp;PM</b> Lunch Break</li>
-                <li><b>1:00 &nbsp;PM</b> <i>Optional Workshop:</i> Build and Deploy Node.js Apps with Microsoft Azure.</li>
-                <li><b>2:00 &nbsp;PM</b> <i>Optional Workshop:</i> CSE 143 Study Group</li>
-                <li><b>2:00 &nbsp;PM</b> <i>Optional Workshop:</i> Python: Basic Training <span class = "dash"></span> Intro to Python Skills for AI</li>
-                <li><b>3:00 &nbsp;PM</b> <i>Optional Workshop:</i> Add an FAQ Bot to Your Webpage with Microsoft Azure</li>
+                <li><b>10:00 AM</b> <span class="lhdWorkshop">iOS Development with Swift and SwiftUI</span></li>
+                <li><b>10:00 AM</b> <span class="lhdStudyGroup">CSE 142 Study Group</span></li>
+                <li><b>11:00 AM</b> <span class="lhdWorkshop">Android Development with Java</span></li>
+                <li><b>11:00 AM</b> <span class="lhdWorkshop">How to Collaborate with GitHub</span></li>
+                <li><b>12:00 PM</b> Lunch Break</li>
+                <li><b>1:00 &nbsp;PM</b> <span class="lhdWorkshop">Algorithms: Hack the Technical Interview</span></li>
+                <li><b>1:00 &nbsp;PM</b> <span class="lhdWorkshop">Build and Deploy Node.js Apps with Microsoft Azure</span></li>
+                <li><b>2:00 &nbsp;PM</b> <span class="lhdStudyGroup">CSE 143 Study Group</span></li>
+                <li><b>2:00 &nbsp;PM</b> <span class="lhdWorkshop">Python: Basic Training <span class = "dash"></span> Intro to Python Skills for AI</span></li>
+                <li><b>3:00 &nbsp;PM</b> <span class="lhdWorkshop">Add an FAQ Bot to Your Webpage with Microsoft Azure</span></li>
                 <li><b>6:00 &nbsp;PM</b> Dinner Break</li>
                 <li><b>8:00 &nbsp;PM</b> End of Hacking, Start of Demos</li>
                 <li><b>8:30 &nbsp;PM</b> Awards!</li>
                 <li><b>9:00 &nbsp;PM</b> Event Ends</li>
             </ul>
-            
-            <h2>Notes</h2>
-            <ul>
-            <li>
-                Many students have a math midterm on December 7<sup>th</sup>!
-                Think of the hackathon as a way to de-stress before or after your midterm.
-                Even several of the organizers will be leaving for midterms! If you need
-                to leave early or arrive late, this is expected. Come.
-            </li>
-            <li>
-                The schedule isn&rsquo;t finished <span class = "dash"></span> it
-                might change before the event.
-            </li>
-            <li>
-                To attend, you must be a student at the University of Washington.
-            </li>
+            <p> The schedule is not final <span class = "dash"></span> it might change before the event without prior notification.</p>
+        </div>
+        
+        <div>
+            <h2>Frequently Asked Questions</h2>
+            <ul class="qaSection">
+                <li>
+                    <details open>
+                        <summary>
+                            I have a math final on that day!
+                        </summary>
+
+                        Many students have a math midterm on December 7<sup>th</sup>! <br> <br>
+                    Think of the hackathon as a way to de-stress before or after your midterm.
+                    Even several of the organizers will be leaving for midterms! If you need
+                    to leave early or arrive late, this is expected. Come.
+                    </details>
+                </li>
+                <li>
+                    <details open>
+                        <summary>
+                            Who can attend?
+                        </summary>
+                            You must be a current student or staff at the University of Washington.
+                    </details>
+                </li>
+                <li>
+                    <details open>
+                        <summary>
+                            Do I have to attend all the workshops/study groups?
+                        </summary>
+                            No! You can work on whatever you want, but bringing a pair of headphones with you is a good idea.
+                    </details>
+                </li>
+                <li>
+                    <details open>
+                        <summary>
+                            How do I win the awards?
+                        </summary>
+                        
+                        We'll announce the award categories later, but it's as easy as showing us the project you worked on <em>during</em> this hackathon!
+                    </details>
+                </li>
+                <li>
+                    <details open>
+                        <summary>
+                            Will there be food?
+                        </summary>
+                        
+                        We recommend brining your own food with you.
+                    </details>
+                </li>
+                <li>
+                    <details open>
+                        <summary>
+                            I have other questions...
+                        </summary>
+
+                        Check the <a href="https://localhackday.mlh.io/build/locations/2874/" target="_blank" rel="noopener noreferrer">registration page FAQ</a>, or contact us through <a href="mailto:appdev@uw.edu">email</a>, <a href="https://twitter.com/uwappdev" target="_blank" rel="noopener noreferrer">twitter</a>, or <a href="https://t.me/UWAppDev" target="_blank" rel="noopener noreferrer">telegram</a>
+                    </details>
+                </li>
             </ul>
-            
-            <sub style = "font-family: sans;">Questions? <a href = "https://huskylink.washington.edu/organization/appdev/">Contact us!</a></sup>
+        </div>
         `,
         
         "Join":
